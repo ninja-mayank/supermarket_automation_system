f5b3c39c2ad30cca579222942a0f98b9578b7a47
ninja-mayank <112952488+ninja-mayank@users.noreply.github.com>
2024-04-18 06:25:22 +0530

    bugs

--- src/app/manager/employees/page.js
+++ src/app/manager/employees/page.js
@@ -7,7 +7,7 @@ const get_employees = async() => {
             <span style={{marginTop: "4.5rem", marginLeft: "17rem"}} className="w-full">
                 <span>
                 {users.map((user) => (    
-                    <a href="#" class="block max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mr-4 mb-4">
+                    <a key={user.id} href="#" class="block max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mr-4 mb-4">
         
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{user.name}</h5>
                     <p class="font-normal text-gray-700">{user.email}</p>
